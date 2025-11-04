373e1c588f73c5646e688ffdb2ba87c92709515e
seehoon <flytofree@p2ach.ai>
2025-11-04 16:16:23 +0900

    versioning

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,6 +6,13 @@ pluginManagement {
         maven("https://raw.githubusercontent.com/saki4510t/libcommon/master/repository/")
         maven("https://jitpack.io")
     }
+
+    plugins {
+
+        id("com.android.library") version "8.5.2"
+        id("org.jetbrains.kotlin.android") version "1.9.24"
+
+    }
 }
 
 dependencyResolutionManagement {
